5beb105fdfaa21ee7e7e4166558814b862821193
Sanket Mistry <sanket.mern.dev@gmail.com>
2025-06-11 10:13:00 +0530

    Setup Order page

--- src/components/common/PageLayout.jsx
+++ src/components/common/PageLayout.jsx
@@ -2,24 +2,18 @@ import React, { Suspense } from "react";
 
 const PageLayout = ({ header: Header, main: Main, footer: Footer }) => {
   return (
-    <div className="absolute top-4 bottom-4 left-4 right-4 flex flex-col">
+    <div className="absolute top-2 bottom-2 left-2 right-2 flex flex-col">
       <header className="p-4 bg-white/70 backdrop-blur-md border border-white shadow-sm text-xl font-semibold text-gray-800 rounded-t-md text-center">
-        <Suspense fallback={<div>Loading header...</div>}>
-          <Header />
-        </Suspense>
+        <Suspense fallback={<div>Loading header...</div>}>{Header}</Suspense>
       </header>
       <div className="flex-1 bg-white/70 backdrop-blur-md border border-white  overflow-y-auto my-0.5">
-        <main className="py-4 px-2 space-y-4 text-gray-700">
-          <Suspense fallback={<div>Loading content...</div>}>
-            <Main />
-          </Suspense>
+        <main className="py-4 px-4 space-y-4 text-gray-700">
+          <Suspense fallback={<div>Loading content...</div>}>{Main}</Suspense>
         </main>
       </div>
 
       <footer className="p-2 bg-white/70 backdrop-blur-md border border-white text-center text-gray-800 rounded-b-md flex items-center justify-around">
-        <Suspense fallback={<div>Loading footer...</div>}>
-          <Footer />
-        </Suspense>
+        <Suspense fallback={<div>Loading footer...</div>}>{Footer}</Suspense>
       </footer>
     </div>
   );
--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,7 +1,7 @@
 import { Button } from "/src/components/ui/button";
 import PageLayout from "/src/components/common/PageLayout";
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { Package } from "lucide-react";
 import { Input } from "/src/components/ui/input";
 
@@ -9,33 +9,50 @@ export const Header = () => {
   return <div>Inventory</div>;
 };
 
-export const Main = () => {
-  const items = [
-    { name: null, buy: null, sell: null },
-    { name: "Macroni", buy: 199, sell: 299 },
-    { name: "Shrikhand", buy: 50, sell: 100 },
-    { name: "Burger", buy: 75, sell: 150 },
-  ];
+export const Main = ({ items, setItems }) => {
+  if (items?.length === 0) {
+    return <>No items added</>;
+  }
+
+  const handleChange = (index, field, value) => {
+    const updatedItems = [...items];
+    updatedItems[index] = {
+      ...updatedItems[index],
+      update: "true",
+      [field]:
+        field === "buy" || field === "sell" ? parseFloat(value) || 0 : value,
+    };
+    setItems(updatedItems);
+  };
+
   return (
     <div className="flex flex-col gap-1">
-      {items.map((item) => (
-        <div className="flex gap-1">
+      <div className="flex gap-1">
+        <div className=" flex-1 px-3 text-sm">Item</div>
+        <div className="w-12 text-center text-sm">Buy</div>
+        <div className="w-12 text-center text-sm">Sell</div>
+      </div>
+      {items.map((item, index) => (
+        <div className="flex gap-1" key={index}>
           <Input
             placeholder="Product name"
             value={item.name}
             className="border-gray-100"
+            onChange={(e) => handleChange(index, "name", e.target.value)}
           />
           <Input
             placeholder="0.00"
             type="number"
             value={item.buy}
             className="w-12 px-1 text-center border-gray-100 text-red-400"
+            onChange={(e) => handleChange(index, "buy", e.target.value)}
           />
           <Input
             placeholder="0.00"
             type="number"
             value={item.sell}
             className="w-12 px-1 text-center border-gray-100 text-green-600"
+            onChange={(e) => handleChange(index, "sell", e.target.value)}
           />
         </div>
       ))}
@@ -43,16 +60,17 @@ export const Main = () => {
   );
 };
 
-export const Footer = () => {
+export const Footer = ({ handleCreateNewItem }) => {
   return (
     <>
       <Button
         variant="ghost"
+        onClick={handleCreateNewItem}
         className="w-full flex items-center gap-3 px-4 py-2 rounded-lg border border-indigo-200 hover:bg-gray-50 hover:text-indigo-600"
       >
         {/* <Icon className="h-5 w-5" /> */}
         <Package />
-        <span>Create New Item</span>
+        Create New Item
       </Button>
       <div className="text-center ml-6 mr-3">
         <span className="underline text-indigo-600">Categories</span>
@@ -63,13 +81,35 @@ export const Footer = () => {
 
 const Inventory = () => {
   const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
   const handleClick = () => {
     localStorage.removeItem("token");
     navigate("/login");
   };
+
+  const newItem = { name: "", buy: "", sell: "", new: true };
+  const [items, setItems] = useState([]);
+
+  const onCreateNewItem = () => {
+    setItems((v) => [newItem, ...v]);
+  };
+
+  useEffect(() => {
+    const createItem = searchParams.get("create-item");
+    if (createItem) {
+      console.log("use effect called....");
+      setItems((v) => [newItem, ...v]);
+    }
+  }, []);
+
   return (
     <>
-      <PageLayout main={Main} footer={Footer} header={Header} />
+      <PageLayout
+        main={<Main items={items} setItems={setItems} />}
+        footer={<Footer handleCreateNewItem={onCreateNewItem} />}
+        header={<Header />}
+      />
     </>
   );
 };
